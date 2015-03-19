9dbf1bec8d89af565953faab526305bf943ca682
Yoshio Kawase <gari2bone.50kg@gmail.com>
2015-03-19 22:18:29 +0900

    リファクタリング、直進対応

--- Assets/Scripts/Stop.cs
+++ Assets/Scripts/Stop.cs
@@ -12,8 +12,6 @@ public class Stop : MonoBehaviour
 
     }
 	void OnTriggerEnter2D(Collider2D col){
-//		col.GetComponent<Rigidbody2D>().gravityScale = 0f;
-//		col.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         if ("stop" == col.name) {
             iTween.Pause(gameObject);
         }
