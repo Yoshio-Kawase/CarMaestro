4a3b23d5eb2eb64c48be4257e7a3548ff8916b01
kazuki-tanaka <kazuki-tanaka@navitime.co.jp>
2015-03-20 21:11:22 +0900

    ゲームオーバー時にゲームオーバー画面に遷移する処理追加

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -4,8 +4,18 @@ using System.Collections;
 public class SwitchScene : MonoBehaviour
 {
 	void OnMouseUpAsButton() {
-		Application.LoadLevel("main");
+		Application.LoadLevel ("main");
 	}
 
+	public Rect zahyou1 = new Rect (     0,   0, 100, 50);
+	public Rect zahyou2 = new Rect (   50,   0, 100, 50);
+	public Rect zahyou3 = new Rect ( 100,   0, 100, 50);
+	
+	void OnGUI()
+	{
+		GUI.Label ( zahyou1, "SCORE");
+		GUI.Label ( zahyou2, "Time");
+		GUI.Label ( zahyou3, "Cube");
+	}
 }
 
